44e8ebe90c1e070f239a9173973d5cb23ae875ae
peco-tina <nemgli-8@student.ltu.se>
2024-01-16 13:48:24 +0100

    Adding a Lambda function on AWS

--- src/main/java/com/membershipsystem/membershipsystem/AWSLambda/LambdaInvoker.java
+++ src/main/java/com/membershipsystem/membershipsystem/AWSLambda/LambdaInvoker.java
@@ -1,5 +1,6 @@
 package com.membershipsystem.membershipsystem.AWSLambda;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
@@ -10,30 +11,31 @@ import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 import java.nio.ByteBuffer;
 
 public class LambdaInvoker {
+    private static final String ACCESS_KEY = "AKIAV3OF5WXD5TBPDWHE";
+    private static final String SECRET_KEY = "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";
+
     public static void invokeLambdaFunction(Long memberId) {
         try {
             String functionName = "arn:aws:lambda:us-east-1:402531399111:function:APICallsRegistration";
 
-            ByteBuffer payloadBuffer = ByteBuffer.allocate(Long.BYTES);
-            payloadBuffer.putLong(memberId);
-            payloadBuffer.flip();
+            AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(ACCESS_KEY, SECRET_KEY);
 
             LambdaClient lambdaClient = LambdaClient.builder()
                     .region(Region.US_EAST_1)
-                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .credentialsProvider(() -> awsCredentials)
                     .build();
+
+            SdkBytes payload = SdkBytes.fromUtf8String(String.valueOf(memberId));
 
             InvokeRequest invokeRequest = InvokeRequest.builder()
                     .functionName(functionName)
-                    .payload(SdkBytes.fromByteBuffer(payloadBuffer))
+                    .payload(payload)
                     .build();
 
             InvokeResponse response = lambdaClient.invoke(invokeRequest);
 
-            // Dodatna logika za rukovanje odgovorom ako je potrebno
             System.out.println("Succeeded with invoking the lambda: " + response);
         } catch (Exception e) {
-            // Uhvatite greške i ispišite ih kako biste ih analizirali
             System.err.println("Error while invoking the lambda: " + e.getMessage());
             e.printStackTrace();
         }
