55babb8ed50458e2ae8aedaa09a348b14b84bd84
peco-tina <nemgli-8@student.ltu.se>
2024-01-16 13:18:37 +0100

    Adding a Lambda function on AWS

--- src/main/java/com/membershipsystem/membershipsystem/AWSLambda/LambdaInvoker.java
+++ src/main/java/com/membershipsystem/membershipsystem/AWSLambda/LambdaInvoker.java
@@ -11,22 +11,31 @@ import java.nio.ByteBuffer;
 
 public class LambdaInvoker {
     public static void invokeLambdaFunction(Long memberId) {
-        String functionName = "arn:aws:lambda:us-east-1:402531399111:function:APICallsRegistration";
+        try {
+            String functionName = "arn:aws:lambda:us-east-1:402531399111:function:APICallsRegistration";
 
-        ByteBuffer payloadBuffer = ByteBuffer.allocate(Long.BYTES);
-        payloadBuffer.putLong(memberId);
-        payloadBuffer.flip();
+            ByteBuffer payloadBuffer = ByteBuffer.allocate(Long.BYTES);
+            payloadBuffer.putLong(memberId);
+            payloadBuffer.flip();
 
-        LambdaClient lambdaClient = LambdaClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
+            LambdaClient lambdaClient = LambdaClient.builder()
+                    .region(Region.US_EAST_1)
+                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .build();
 
-        InvokeRequest invokeRequest = InvokeRequest.builder()
-                .functionName(functionName)
-                .payload(SdkBytes.fromByteBuffer(payloadBuffer))
-                .build();
+            InvokeRequest invokeRequest = InvokeRequest.builder()
+                    .functionName(functionName)
+                    .payload(SdkBytes.fromByteBuffer(payloadBuffer))
+                    .build();
 
-        InvokeResponse response = lambdaClient.invoke(invokeRequest);
+            InvokeResponse response = lambdaClient.invoke(invokeRequest);
+
+            // Dodatna logika za rukovanje odgovorom ako je potrebno
+            System.out.println("Succeeded with invoking the lambda: " + response);
+        } catch (Exception e) {
+            // Uhvatite greške i ispišite ih kako biste ih analizirali
+            System.err.println("Error while invoking the lambda: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
